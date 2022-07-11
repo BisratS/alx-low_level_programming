69ed05bb6095d9f5383a4ef507f422438677007d
Bisrat Shiferaw <57441461+BisratS@users.noreply.github.com>
2022-07-11 11:42:18 +0300

    fix function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,9 +12,7 @@
 
 void _puts(char *str)
 {
-	int count;
-
-	count = 0;
+	int count = 0;
 
 	while (count >= 0)
 	{
@@ -22,9 +20,8 @@ void _puts(char *str)
 		{
 			_putchar('\n');
 			break;
-		
 		}
+		_putchar(str[count]);
+		count++;
 	}
-	_putchar(str[count]);
-	count++;
 }
