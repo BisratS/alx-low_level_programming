bcc882681918b973b53a1b5132e7c090d33e72c3
BisratS <bisratjdb@gmail.com>
2022-07-11 04:53:10 -0700

    function that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ *
+ * @s: string to be converted to integer
+ *
+ * Return: no return
+ *
+ */
+
+int _atoi(char *s)
+{
+	int val;
+
+	val = atoi(s);
+
+	return (val);
+}
