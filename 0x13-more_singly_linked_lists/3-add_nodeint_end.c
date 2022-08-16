2b14195867355bcbf72a964d2d7c4a7be9cb14ba
BisratS <bisratjdb@gmail.com>
2022-08-15 23:58:55 -0700

    fix 3-add try

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  * Return: the address of the new element, or NULL if it failed
  */
 
-stint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp;
